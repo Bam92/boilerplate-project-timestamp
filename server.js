198023c9c7ca8a13a73ecdaf23c86ea82949a537
abelmbula <abelmbula@bam.com>
2019-11-25 16:08:47 +0200

    Add new routes to handle user date

--- server.js
+++ server.js
@@ -24,9 +24,18 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp", (req, res) => {
+  const userDate = new Date();
+  res.json({  unix: userDate.getTime(), utc: userDate.toUTCString() })
 
+});
+
+app.get("/api/timestamp/:date_string", function (req, res) {
+  const userDate = new Date(req.params.date_string) || new Date(req.params.date_string * 1000);
+  res.json({  unix: userDate.getTime(), utc: userDate.toUTCString() })
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 4000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
